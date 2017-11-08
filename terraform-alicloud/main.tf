da4a7985eefdba387d89e8b9cbcb7727c02d87e7
Zeyu Chen <xinyifly@live.cn>
2017-11-09 01:46:41 +0800

    refactor(terraform): remove io_optimized, system_disk_category

--- terraform-alicloud/main.tf
+++ terraform-alicloud/main.tf
@@ -7,10 +7,7 @@ resource "alicloud_security_group" "blizzard" {}
 resource "alicloud_instance" "blizzard" {
   image_id = "ubuntu_16_0402_64_20G_alibase_20170818.vhd"
   instance_type = "ecs.xn4.small"
-  io_optimized = "optimized"
   security_groups = ["${alicloud_security_group.blizzard.id}"]
-
-  system_disk_category = "cloud_efficiency"
 
   allocate_public_ip = true
   internet_max_bandwidth_out = 1
